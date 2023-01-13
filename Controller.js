5aa3d61f80fb19c16abd78852d2007690ac61555
Axelle <axelle.donot@hotmail.fr>
2023-01-13 11:06:03 +0100

    fix(): amélioration graphique

--- Controller.js
+++ Controller.js
@@ -6,117 +6,122 @@ class Controller {
   }
 
   creationGrille() {
-    this.timing=false;
-    this.fini=false;
+    this.timing = false;
+    tps = 1500;
+    this.fini = false;
     this.grille = new ModelGrille();
     this.creationTetrominos();
     lancement();
-    this.boolDescente=true
+    this.boolDescente = true;
   }
   creationTetrominos() {
-    if(!this.fini && !this.timing){
-        this.tetrominos = new ModelTetrominos();
-        this.grille.ajouterTetrominos(this.tetrominos);
-        afficherGrille();
-        this.verifierFinGrille()
+    if (!this.fini && !this.timing) {
+      this.tetrominos = new ModelTetrominos();
+      this.grille.ajouterTetrominos(this.tetrominos);
+      afficherGrille();
+      this.verifierFinGrille();
     }
   }
 
   descendreRapidement() {
-   if(!this.fini){
-      if(!this.grille.verifTetrominos(this.tetrominos) && this.boolDescente){
-          this.boolDescente=false;
-          let interval = setInterval(function(){
-            app.grille.descendre(app.tetrominos);
-            descenteTetrominos(app.tetrominos)
-            let val = app.grille.verifTetrominos(app.tetrominos)
-            if(val || app.tetrominos.coordonnes[0]+app.tetrominos.nbLigne-1 >= 24){
-              clearInterval(interval)
-              app.boolDescente=true
-              app.verifierLigneEntiere()
-              app.verifierFinGrille()
-            }
-          },40)
-      }else{
-        this.verifierLigneEntiere()
-        this.verifierFinGrille()
+    if (!this.fini) {
+      if (!this.grille.verifTetrominos(this.tetrominos) && this.boolDescente) {
+        this.boolDescente = false;
+        let interval = setInterval(function () {
+          app.grille.descendre(app.tetrominos);
+          descenteTetrominos(app.tetrominos);
+          let val = app.grille.verifTetrominos(app.tetrominos);
+          if (
+            val ||
+            app.tetrominos.coordonnes[0] + app.tetrominos.nbLigne - 1 >= 24
+          ) {
+            clearInterval(interval);
+            app.boolDescente = true;
+            app.verifierLigneEntiere();
+            app.verifierFinGrille();
+          }
+        }, 40);
+      } else {
+        this.verifierLigneEntiere();
+        this.verifierFinGrille();
       }
     }
   }
 
-  
   descendre() {
-    if(!this.fini){
-      if(!this.grille.verifTetrominos(this.tetrominos)){
+    if (!this.fini) {
+      if (!this.grille.verifTetrominos(this.tetrominos)) {
         this.grille.descendre(this.tetrominos);
-        this.verifierLigneEntiere()
-      }else{
-        this.verifierLigneEntiere()
+        this.verifierLigneEntiere();
+      } else {
+        this.verifierLigneEntiere();
       }
 
       afficherGrille();
-      this.verifierFinGrille()
+      this.verifierFinGrille();
     }
   }
 
   droite() {
-    if(!this.fini){
-      if(!this.grille.verifTetrominos(this.tetrominos)){
-      this.grille.deplacerDroite(this.tetrominos);
-      }else{
-        this.verifierLigneEntiere()
+    if (!this.fini) {
+      if (!this.grille.verifTetrominos(this.tetrominos)) {
+        this.grille.deplacerDroite(this.tetrominos);
+      } else {
+        this.verifierLigneEntiere();
       }
       afficherGrille();
-      this.verifierFinGrille()
+      this.verifierFinGrille();
     }
   }
 
   gauche() {
-    if(!this.fini){
-      if(!this.grille.verifTetrominos(this.tetrominos)){
+    if (!this.fini) {
+      if (!this.grille.verifTetrominos(this.tetrominos)) {
         this.grille.deplacerGauche(this.tetrominos);
-      }else{
-        this.verifierLigneEntiere()
+      } else {
+        this.verifierLigneEntiere();
       }
       afficherGrille();
-      this.verifierFinGrille()
+      this.verifierFinGrille();
     }
-
   }
 
   score() {
-    if(this.grille.score>0){
-      tps = (1/(this.grille.score*20))*1800000
-      lancement()
+    if (this.grille.score > 0) {
+      tps = (1 / (this.grille.score * 20)) * 1800000;
+      lancement();
     }
     return this.grille.score;
   }
 
   verifierLigneEntiere() {
     this.ligneASupp = this.grille.verifLigne();
-    if (this.ligneASupp != false && this.grille.verifTetrominos(this.tetrominos)) {
-      this.timing=true;
-      supprimerLignes(this.ligneASupp)
-      setTimeout(function() {
-        app.timing=false;
+    if (
+      this.ligneASupp != false &&
+      this.grille.verifTetrominos(this.tetrominos)
+    ) {
+      this.timing = true;
+      supprimerLignes(this.ligneASupp);
+      setTimeout(function () {
+        app.timing = false;
         app.grille.suppLigne(app.ligneASupp);
-        app.ligneASupp=false
-    },200)
-     this.creationTetrominos();
-    }else if (this.grille.verifTetrominos(this.tetrominos)){
+        app.ligneASupp = false;
+      }, 200);
+      this.creationTetrominos();
+    } else if (this.grille.verifTetrominos(this.tetrominos)) {
       this.creationTetrominos();
     }
   }
 
-  verifierFinGrille(){
-    if(this.grille.verifGrilleFini(this.tetrominos)){
-      this.fini=true
-      arret()
+  verifierFinGrille() {
+    if (this.grille.verifGrilleFini(this.tetrominos)) {
+      this.fini = true;
+      arret();
     }
   }
 
   rotation() {
-    this.grille.rotation(this.tetrominos)
-    afficherGrille()
+    this.grille.rotation(this.tetrominos);
+    afficherGrille();
   }
 }
--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
-//const largeur = 25;
-var tps = 1500
-var interval=0;
+const largeur = 20;
+var tps = 1500;
+var interval = 0;
 // changement de façon de faire
 // lire chaque case et hop le rectangle est fait selon la couleur
 couleur = {
@@ -17,178 +17,173 @@ var context = canvas.getContext("2d");
 var context2 = canvas.getContext("2d");
 var contextGrille = canvas.getContext("2d");
 
+creerGrille();
+
 function creerGrille() {
   contextGrille = canvas.getContext("2d");
 
   contextGrille.strokeStyle = "White";
-  contextGrille.fillStyle = 'white';
+  contextGrille.fillStyle = "white";
   contextGrille.font = "40px serif";
   contextGrille.fillText("Tetris", 25, 40);
-  contextGrille.strokeStyle = "green";
+  contextGrille.strokeStyle = "white";
   contextGrille.lineWidth = 5;
   contextGrille.strokeRect(100, 50, largeur * 10, largeur * 25);
-  contextGrille.strokeRect(400, 50, largeur * 4, largeur * 2);
   contextGrille.font = "20px serif";
-  contextGrille.fillText("SCORE", 400, 200);
-  contextGrille.fillText("Active IA", 400, 300);
-  
+  contextGrille.fillText("SCORE", 400, 100);
+  contextGrille.fillText("Active IA", 400, 200);
+
   contextGrille.beginPath();
-  contextGrille.strokeStyle = "black";
-  contextGrille.lineWidth = 1;
+  contextGrille.strokeStyle = "grey";
+  contextGrille.lineWidth = 0.1;
   for (let i = 0; i < 475; i += 20) {
-    contextGrille.moveTo(100, 70 + i);
-    contextGrille.lineTo(300, 70 + i);
+    contextGrille.moveTo(103, 70 + i);
+    contextGrille.lineTo(297, 70 + i);
     contextGrille.stroke();
   }
   for (let j = 0; j < 180; j += 20) {
-    contextGrille.moveTo(120 + j, 50);
-    contextGrille.lineTo(120 + j, 550);
+    contextGrille.moveTo(120 + j, 53);
+    contextGrille.lineTo(120 + j, 547);
     contextGrille.stroke();
   }
-
 }
-
 
 function afficherGrille() {
   context.clearRect(0, 0, canvas.width, canvas.height);
-    creerGrille()
-    for (let i = 0; i < 25; i++) {
-      for (let j = 0; j < 10; j++) {
-        if (app.grille.matrice[i][j] > 0) {
-          context.strokeStyle = couleur[app.grille.matrice[i][j]];
-          context.strokeRect(
-            100+largeur * j+2 ,
-            50+largeur * i +2,
-            largeur - 4,
-            largeur - 4
-          );
-        }
+  creerGrille();
+  for (let i = 0; i < 25; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (app.grille.matrice[i][j] > 0) {
+        context.strokeStyle = couleur[app.grille.matrice[i][j]];
+        context.lineWidth = 2;
+        context.strokeRect(
+          100 + largeur * j + 2,
+          50 + largeur * i + 2,
+          largeur - 4,
+          largeur - 4
+        );
       }
+    }
   }
-  score()
+  score();
 }
 
 document.addEventListener("keydown", function (event) {
   if (event.code == "ArrowRight" && !app.fini && app.boolDescente) {
     app.droite();
     app.verifierLigneEntiere();
-
   } else if (event.code == "ArrowLeft" && !app.fini && app.boolDescente) {
     app.gauche();
     app.verifierLigneEntiere();
-
   } else if (event.code == "ArrowDown" && !app.fini && app.boolDescente) {
     app.descendre();
     app.verifierLigneEntiere();
-
   } else if (event.code == "Space" && !app.fini && app.boolDescente) {
     app.descendreRapidement();
-  }else if (event.code == "ArrowUp" && !app.fini && app.boolDescente) {
+  } else if (event.code == "ArrowUp" && !app.fini && app.boolDescente) {
     app.rotation();
     app.verifierLigneEntiere();
   }
-
 });
 
-function descenteTetrominos(tetrominos){
-  let t = tetrominos
-  let tabI = new Array (4);
-  let tabJ = new Array (4);
-  let cpt=0;
-  for(let i=0;i<t.nbLigne;i++){
-    for(let j=0;j<t.nbColonne;j++){
-      if(t.matricePetite[i][j]>0){
-        tabI[cpt]=i+t.coordonnes[0];
-        tabJ[cpt]=j+t.coordonnes[1];
+function descenteTetrominos(tetrominos) {
+  let t = tetrominos;
+  let tabI = new Array(4);
+  let tabJ = new Array(4);
+  let cpt = 0;
+  for (let i = 0; i < t.nbLigne; i++) {
+    for (let j = 0; j < t.nbColonne; j++) {
+      if (t.matricePetite[i][j] > 0) {
+        tabI[cpt] = i + t.coordonnes[0];
+        tabJ[cpt] = j + t.coordonnes[1];
         cpt++;
       }
     }
   }
-  cpt=0
+  cpt = 0;
   context.clearRect(0, 0, canvas.width, canvas.height);
   context2.clearRect(0, 0, canvas.width, canvas.height);
-  creerGrille()
+  creerGrille();
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 10; j++) {
-      if(cpt<4 && tabI[cpt]==i && tabJ[cpt]==j){
-        context2.fillStyle=couleur[app.grille.matrice[i][j]];
+      if (cpt < 4 && tabI[cpt] == i && tabJ[cpt] == j) {
+        context2.fillStyle = couleur[app.grille.matrice[i][j]];
         context2.fillRect(
-          100+largeur * j + 2,
-          50+largeur * i + 2,
+          100 + largeur * j + 2,
+          50 + largeur * i + 2,
           largeur - 4,
           largeur - 4
         );
-        cpt++
-      }else if (app.grille.matrice[i][j] > 0) {
+        cpt++;
+      } else if (app.grille.matrice[i][j] > 0) {
         context.strokeStyle = couleur[app.grille.matrice[i][j]];
+        context.lineWidth = 2;
         context.strokeRect(
-          100+largeur * j + 2,
-          50+largeur * i + 2,
+          100 + largeur * j + 2,
+          50 + largeur * i + 2,
           largeur - 4,
           largeur - 4
         );
       }
     }
   }
-  score()
+  score();
 }
 
-function supprimerLignes(tabLignes){
+function supprimerLignes(tabLignes) {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  creerGrille()
+  creerGrille();
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 10; j++) {
-      if(tabLignes.includes(i)){
+      if (tabLignes.includes(i)) {
         context.fillRect(
-          100+largeur * j + 2,
-          50+largeur * i + 2,
+          100 + largeur * j + 2,
+          50 + largeur * i + 2,
           largeur - 4,
           largeur - 4
         );
-      }else if (app.grille.matrice[i][j] > 0) {
+      } else if (app.grille.matrice[i][j] > 0) {
         context.strokeStyle = couleur[app.grille.matrice[i][j]];
+        context.lineWidth = 2;
         context.strokeRect(
-          100+largeur * j + 2,
-          50+largeur * i + 2,
+          100 + largeur * j + 2,
+          50 + largeur * i + 2,
           largeur - 4,
           largeur - 4
         );
       }
     }
   }
-  score()
+  score();
 }
 
-  function score(){
-    contextGrille.font = "20px serif";
-    contextGrille.fillStyle = 'white';
-    contextGrille.fillText(app.score(), 400, 220)
-  }
+function score() {
+  contextGrille.font = "20px serif";
+  contextGrille.fillStyle = "white";
+  contextGrille.fillText(app.score(), 400, 120);
+}
 
-  //faire descendre les cases toutes les tps
-  function lancement(){
-     if(interval!==undefined){
-      clearInterval(interval)
-     }
-     interval = setInterval(function(){
-      if(app.boolDescente){
-        app.descendre()
-        app.verifierLigneEntiere();
-      }
-      },tps); 
+//faire descendre les cases toutes les tps
+function lancement() {
+  if (interval !== undefined) {
+    clearInterval(interval);
+  }
+  interval = setInterval(function () {
+    if (app.boolDescente) {
+      app.descendre();
+      app.verifierLigneEntiere();
     }
-     
-  function arret(){
-    clearInterval(arret)
-    var contextFin= canvas.getContext("2d");
-    var contextText =canvas.getContext("2d");
-    contextFin.fillStyle="grey"
+  }, tps);
+}
 
-    contextFin.fillRect(50, 150, largeur * 20, largeur * 10);
-    contextText.fillStyle = 'Black';
-    contextText.font = "20px serif";
-    contextText.fillText("VOUS AVEZ PERDU", 150,250);
+function arret() {
+  clearInterval(arret);
+  var contextFin = canvas.getContext("2d");
+  var contextText = canvas.getContext("2d");
+  contextFin.fillStyle = "grey";
 
-
-  }
-
+  contextFin.fillRect(50, 150, largeur * 20, largeur * 10);
+  contextText.fillStyle = "Black";
+  contextText.font = "20px serif";
+  contextText.fillText("VOUS AVEZ PERDU", 150, 250);
+}
