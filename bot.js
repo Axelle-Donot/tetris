45fd4ebc661451809efb5c71632cc69f870d85e8
Axelle <axelle.donot@hotmail.fr>
2023-01-19 11:28:10 +0100

    calcule du scores

--- bot.js
+++ bot.js
@@ -7,6 +7,45 @@ for (let i = 0; i < taillePop; i++) {
   population.push((appBot.model.grille = new ModelGrille()));
 }
 
+const score = (grille) => {
+  var minligne = 0;
+  var maxligne = 0;
+  var nombreligne = 0;
+  var nombretroue = 0;
+
+  for (let i = 0; i < 25; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (grille[i][j] !== 0) {
+        maxligne = i;
+        i = 25;
+        j = 10;
+      }
+    }
+  }
+
+  nombreligne = appBot.model.grille.verifLigne();
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 25; j++) {
+      if (grille[j][i] !== 0) {
+        if (j < minligne) {
+          minligne = j;
+        }
+      }
+    }
+  }
+
+  for (let i = 24; i <= 0; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (grille[i][j] === 0 && grille[i + 1][j] === 1) {
+        nombretroue += 1;
+      }
+    }
+  }
+
+  return nombreligne + nombretroue + maxligne + (maxligne - minligne);
+};
+
 for (let i = 0; i < taillePop; i++) {
   tetrominos = new ModelTetrominos();
 
@@ -16,49 +55,44 @@ for (let i = 0; i < taillePop; i++) {
     scores[j] = new Array(10);
   }
 
-  // position des tetrominos
- // population[i].ajouterTetrominos(tetrominos);
-
-  console.log({ pop: population[i] });
-
-  for (let k = 0; k < 1; k++) {
+  for (let k = 0; k < 4; k++) {
     for (let l = 0; l < scores[k].length; l++) {
-      console.log("----------------------------------------");
       tetrominos.coordonnes = [0, l];
       population[i].ajouterTetrominos(tetrominos);
 
-      console.log(tetrominos.coordonnes);
-  
-      console.log(mat)
-        population[i].descendreRapidement(tetrominos);
-  
+      population[i].descendreRapidement(tetrominos);
 
-      console.log(k, l);
-      console.log(tetrominos.coordonnes);
+      scores[k][l] = score(population[i].matrice);
+      console.log(scores[k][l]);
 
-      console.log("----------------------------------------");
-      //calcule scors
-      //suppr le tetrominos
       population[i].supprimerTetrominos(tetrominos);
 
-      console.log("----------------------------------------");
-
-
       tetrominos.coordonnes = [0, 0];
-
     }
-    //population[i].rotation(tetrominos);
+    population[i].rotation(tetrominos);
   }
+
+  var bestScore = scores[0][0];
+  var ligne = 0;
+  var colone = 0;
+
+  console.log(scores);
+
+  for (let m = 0; m < 4; m++) {
+    for (let n = 0; n < scores[m].length; n++) {
+      console.log(scores[m][n], m, n);
+      if (scores[m][n] < bestScore) {
+        bestScore = scores[m][n];
+        ligne = m;
+        colone = n;
+      }
+    }
+  }
+
+  console.log(bestScore);
+  console.log(ligne, colone);
+
+  //ajouter piece au bonne endroit
 }
-
-const score = (grille, ligne) => {
-  const minligne = 0;
-  const maxligne = 0;
-
-  //ligne la plus haut
-  //nombre ligne complete
-  //nombre de trous
-  //Variation hauteur
-};
 
 console.log({ pop: population });
