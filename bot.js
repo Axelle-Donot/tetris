015983dfea153d089ef2331498fab9748951ac8d
Axelle <axelle.donot@hotmail.fr>
2023-01-22 16:43:07 +0100

    avacement du bot

--- bot.js
+++ bot.js
@@ -1,7 +1,28 @@
 const appBot = new Controller(new Model(), new View());
 
 const taillePop = 10;
+
 var population = Array();
+var scoresPop = new Array(taillePop);
+var coefPop = new Array(taillePop);
+for (var j = 0; j < taillePop; j++) {
+  coefPop[j] = new Array(4);
+}
+
+for (var j = 0; j < taillePop; j++) {
+  coefPop[j][0] = 2;
+  coefPop[j][1] = 1;
+  coefPop[j][2] = 1;
+  coefPop[j][3] = 1;
+}
+
+/**
+ * CoefPop : coefficiant de chaque individue
+ * coefPop[individue][0] = coeficient du nombre de troue
+ * coefPop[individue][1] = coeficient de la ligne max
+ * coefPop[individue][2] = coeficient de la variation de hauteur
+ * coefPop[individue][3] = coeficient du nombre de lignes
+ */
 
 for (let i = 0; i < taillePop; i++) {
   population.push((appBot.model.grille = new ModelGrille()));
@@ -17,7 +38,7 @@ const hauteur = (grille, c) => {
   return h;
 };
 
-const score = (grille) => {
+const score = (grille, indice) => {
   var difference = 0;
   var maxligne = 24;
   var nombreligne = 0;
@@ -48,107 +69,122 @@ const score = (grille) => {
     }
   }
 
-  // for (let i = 0; i < 10; i++) {
-  //   for (let j = 0; j < 24; j++) {
-  //     if (grille[j][i] !== 0) {
-  //       for (let b = j + 1; b <= 24; b++) {
-  //         if (grille[b][i] === 0) {
-  //           console.log({ b: b, i: i });
-  //           console.log({ piece: grille[j][i] });
-  //           console.log({ dessous: grille[b][i] });
-  //           nombretroue++;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
   // console.log({ nombreligne: nombreligne });
   // console.log({ nombretroue: nombretroue });
   // console.log({ maxligne: maxligne });
   // console.log({ difference: difference });
 
-  return nombretroue * 2 + (25 - maxligne) - nombreligne;
+  // console.log(coefPop);
+
+  return (
+    nombretroue * coefPop[indice][0] +
+    (25 - maxligne) * coefPop[indice][1] +
+    difference * coefPop[indice][2] -
+    nombreligne * coefPop[indice][3]
+  );
 };
 
-const generation = 50;
+const nbPiece = 200;
+const generation = 5;
 
-for (let h = 0; h < generation; h++) {
-  for (let i = 0; i < taillePop; i++) {
-    tetrominos = new ModelTetrominos();
-    console.log(tetrominos.matrice);
+for (let c = 0; c < generation; c++) {
+  for (let h = 0; h < nbPiece; h++) {
+    for (let i = 0; i < taillePop; i++) {
+      tetrominos = new ModelTetrominos();
+      // console.log(tetrominos.matrice);
 
-    // création matrice score
-    var scores = new Array(4);
-    for (var j = 0; j < 4; j++) {
-      scores[j] = new Array(
-        j % 2 === 0
-          ? 10 - tetrominos.matricePetite[0].length + 1
-          : 10 - tetrominos.matricePetite.length + 1
-      );
-    }
+      // création matrice score
+      var scores = new Array(4);
+      for (var j = 0; j < 4; j++) {
+        scores[j] = new Array(
+          j % 2 === 0
+            ? 10 - tetrominos.matricePetite[0].length + 1
+            : 10 - tetrominos.matricePetite.length + 1
+        );
+      }
 
-    for (let k = 0; k < 4; k++) {
-      for (let l = 0; l < scores[k].length; l++) {
-        tetrominos.coordonnes = [0, l];
+      for (let k = 0; k < 4; k++) {
+        for (let l = 0; l < scores[k].length; l++) {
+          tetrominos.coordonnes = [0, l];
+          population[i].ajouterTetrominos(tetrominos);
+
+          population[i].descendreRapidement(tetrominos);
+
+          scores[k][l] = score(population[i].matrice, i);
+
+          population[i].supprimerTetrominos(tetrominos);
+        }
+        tetrominos.coordonnes = [0, 0];
         population[i].ajouterTetrominos(tetrominos);
-
-        population[i].descendreRapidement(tetrominos);
-
-        scores[k][l] = score(population[i].matrice);
-
+        population[i].rotation(tetrominos);
         population[i].supprimerTetrominos(tetrominos);
       }
-      tetrominos.coordonnes = [0, 0];
-      population[i].ajouterTetrominos(tetrominos);
-      population[i].rotation(tetrominos);
-      population[i].supprimerTetrominos(tetrominos);
-    }
 
-    var bestScore = scores[0][0];
-    var rotation = 0;
-    var colone = 0;
+      var bestScore = scores[0][0];
+      var rotation = 0;
+      var colone = 0;
 
-    for (let m = 0; m < 4; m++) {
-      for (let n = 0; n < scores[m].length; n++) {
-        if (scores[m][n] < bestScore) {
-          bestScore = scores[m][n];
-          rotation = m;
-          colone = n;
+      for (let m = 0; m < 4; m++) {
+        for (let n = 0; n < scores[m].length; n++) {
+          if (scores[m][n] < bestScore) {
+            bestScore = scores[m][n];
+            rotation = m;
+            colone = n;
+          }
         }
       }
+
+      // console.log(scores);
+
+      tetrominos.coordonnes = [0, colone];
+
+      population[i].ajouterTetrominos(tetrominos);
+
+      for (let o = 0; o < rotation; o++) {
+        population[i].rotation(tetrominos);
+      }
+
+      population[i].descendreRapidement(tetrominos);
+
+      population[i].suppLigne(population[i].verifLigne());
+
+      population[i].verifGrilleFini(tetrominos);
+
+      // let vide = "";
+      // for (let z = 0; z < 25; z++) {
+      //   for (let j = 0; j < 10; j++) {
+      //     vide += population[i].matrice[z][j] + " ";
+      //   }
+      //   vide += "\n";
+      // }
+
+      // console.log(vide);
     }
-
-    console.log(scores);
-
-    tetrominos.coordonnes = [0, colone];
-
-    population[i].ajouterTetrominos(tetrominos);
-
-    for (let o = 0; o < rotation; o++) {
-      population[i].rotation(tetrominos);
-    }
-
-    population[i].descendreRapidement(tetrominos);
-
-    population[i].suppLigne(population[i].verifLigne());
-
-    population[i].verifGrilleFini(tetrominos);
-
-    // let vide = "";
-    // for (let z = 0; z < 25; z++) {
-    //   for (let j = 0; j < 10; j++) {
-    //     vide += population[i].matrice[z][j] + " ";
-    //   }
-    //   vide += "\n";
-    // }
-
-    // console.log(vide);
   }
-}
 
-console.log({ pop: population });
+  console.log({ pop: population });
 
-for (let i = 0; i < taillePop; i++) {
-  console.log(score(population[i].matrice));
+  for (let i = 0; i < taillePop; i++) {
+    scoresPop[i] = score(population[i].matrice, i);
+  }
+
+  let maxScore = 0;
+  let maxindice = 0;
+  for (let i = 0; i < taillePop; i++) {
+    if (scoresPop[i] < maxScore) {
+      maxScore = scoresPop[i];
+      maxindice = i;
+    }
+  }
+
+  let maxScore2 = 0;
+  let maxindice2 = 0;
+  for (let i = 0; i < taillePop; i++) {
+    if (scoresPop[i] < maxScore && i !== maxindice) {
+      maxScore2 = scoresPop[i];
+      maxindice2 = i;
+    }
+  }
+
+  // faire la moyenne des deux meilleurs
 }
